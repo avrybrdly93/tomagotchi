24ee82e499937b00c2ecabc8d59cca0a4e5db18e
Avery <cbra067@lasierra.edu>
2018-12-08 13:44:16 -0800

    included basic command-line stuffs

--- t.js
+++ t.js
@@ -1,54 +1,10 @@
-//  <!-- # **Instructions**
-
-// * Remember Tamagotchis? They were those little toys that contained "digital pets" which you could feed, pet, play with, and care for. Kind of like Furbies but a whole lot less terrifying. Over the next thirty minutes or so, you are going to create your own basic Tamagotchi clone using constructors.
-
-// * Create a constructor called "Tomagotchi" which will create four properties and four methods...
-
-//   * The first property is "hungry" and it initially starts out as false
-
-//   * The second property is "sleepy" and it initially starts out as false
-
-//   * The third property is "bored" and it initially starts out as true
-
-//   * The fourth property is "age" and it initially starts out at 0
-
-// * The first method is "feed()" - If hungry is true, print "That was yummy!", set hungry to false, and then set sleepy to true. If hungry is false, print "No thanks! I'm full."
-
-// * The second method is "sleep()" - If sleepy is true, print `Zzzzzzzz`, set sleepy to false, then set bored to true, and then run increaseAge(). If sleepy is false, print "No way! I'm not tired."
-
-// * The third method is "play()" - If bored is true, print "Yay! Let's play!", set bored to false, and then set hungry to true. If bored is false, print "Not right now. Later?"
-
-// * The fourth method is "increaseAge()" - This method is called by the sleep() method when the Tomagotchi goes to sleep and increases the Tomagotchi's age by one while also printing "Happy Birthday to me! I am "+age+" old!"
-
-// * Create a variable named "dog" that is set to a new Tomagotchi before adding the following unique properties/methods to it...
-
-//   * outside - Initially set to false
-
-//   * bark() - Prints out "Woof! Woof!" when run
-
-//   * goOutside() - If outside is false, prints "Yay! I love the outdoors!", sets outside to true, and runs bark(). If outside is true, prints "We're already outside though..."
-
-//   * goInside() - If outside is true, prints "Do we have to? Fine..." and sets outside to false. If outside is false, prints "I'm already inside..."
-
-// * Make a second variable named "cat" that is set to a new Tomagotchi and add the following methods to it:
-
-//   * houseCondition - Initially set to 100... But not for long...
-
-//   * meow() - prints out "Meow! Meow!" when run
-
-//   * destroyFurniture() - Decreases houseCondition by 10 and prints "MUAHAHAHAHA! TAKE THAT FURNITURE!" to the screen. Also sets bored to false and sleepy to true. If houseCondition is equal to 0, then this should not run anymore.
-
-//   * buyNewFurniture() - Increases houseCondition by 50 and prints "Are you sure about that?" to the screen.
-
-// * Play around with your newly created digital pets for a bit and see what else you could add in order to make them even more exciting!
-
-// * BONUS: Make it so your Tamagotchis run off of user-input from the command line. It may sound easy at first, but this can actually be quite challenging. We will be going over this in more detail during the next class. -->
-
-function Tomagotchi ( hungry, sleepy, bored, age, ) {
+var inquirer = require('inquirer');
+function Tomagotchi ( hungry, sleepy, bored, age, name) {
     this.hungry = hungry,
     this.sleepy = sleepy,
-    this.bored = bored
-    this.age =  age
+    this.bored = bored,
+    this.age =  age,
+    this.name = name,
 
 
     this.Feed = function (){
@@ -86,9 +42,7 @@ function Tomagotchi ( hungry, sleepy, bored, age, ) {
     
     this.increaseAge = function(){
         console.log("Happy Birthday to me! I am " + this.age + "old.");
-    };
-    
-    
+    }; 
 }
 
 let dog = new Tomagotchi();
@@ -96,8 +50,6 @@ dog.outside = false;
 dog.bark = function() {
     console.log("Woof Woof");
 }
-
-dog.bark();
 dog.goOutside = function() {
     if(this.outside === false) {
         console.log("Yay! I love the outdoors!");
@@ -132,5 +84,51 @@ cat.buyNewfurniture = function() {
     console.log("Are you sure?");
 }
 
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is the name of your Tomagotchi?",
+        name: "tName"
+    },
+    {
+        type: "list",
+        message: "Is your tomagotchi hungry?",
+        choices: ["yes", "no"],
+        name: "tHungry"
+    },
+    {
+        type: "list",
+        message: "Is your tomagotchi sleepy?",
+        choices: ["yes", "no"],
+        name: "tSleepy"
+    },
+    {
+        type: "list",
+        message: "is your tomagotchi bored?",
+        choices: ["yes", "no"],
+        name: "tBored"
+    },
+    {
+        type: "input",
+        message: "How old is your tomagotchi?",
+        name: "tAge"
+    }
 
+]).then(function(response) {
+    let userTomagotchi = new Tomagotchi(response.tHungry, response.tSleepy, response.tBored, response.tAge, response.tName);
+    console.log("Your tomagotchi's name is " + userTomagotchi.name);
+    yesOrNo(userTomagotchi.hungry, "Yes, your tomagotchi is hungry", "No, your tomagotchi isn't hungry :(");
+    yesOrNo(userTomagotchi.sleepy, "Yes, your tomagotchi is sleepy", "No, your tomagotchi isn't sleepy :(");
+    yesOrNo(userTomagotchi.bored, "Yes, your tomagotchi is bored", "No, your tomagotchi isn't bored :(");
+    console.log("Your tomagotchi's age is " + userTomagotchi.age);
+});
+
+function yesOrNo(tomagotchiStatus, tomagotchiYes, tomagotchiNo) {
+    if(tomagotchiStatus === "yes") {
+        console.log(tomagotchiYes);
+    }
+    else {
+        console.log(tomagotchiNo);
+    }
+}
     
